101b5be39b72d8ed22730c4188414ce52f9f943b
Henning <henning.holgersen@gmail.com>
2023-03-28 22:13:21 +0200

    valid openlineage schema and split logs into runs

--- produce_metadata.py
+++ produce_metadata.py
@@ -3,6 +3,7 @@ import json
 import uuid
 import argparse
 import os
+import re
 
 def find_element(name, data):
     for element in data:
@@ -25,33 +26,79 @@ def get_values(name, data):
     return values
 
 
-def parse_logs(filepath, manifest_path):
+def emit_openlineage_from_summary(run_summary):
+  start_record = {
+                  "eventType": "START", 
+                  "eventTime": run_summary["start_time"],
+                  "run": {
+                    "runId": run_summary["run"]["runId"],
+                  },
+                  "job": run_summary["job"],
+                  "inputs": run_summary["inputs"],
+                  "outputs": run_summary["outputs"],
+                  "producer": "https://meltano.com"
+                }
 
-  d = {
-      "producer_name": None, 
-      "consumer_name": None, 
-      "streams": [],
-      "inputs": [],
-      "outputs": [],
-      "run": {
-        "runId": str(uuid.uuid4()),
-        "metrics": {}
-      },
-      "job": {
-      "namespace": None,
-      "name": None,
-      },
-      "start_time": None, 
-      "end_time": None,
+  if run_summary["success"]:
+    event_type = "COMPLETE"
+  else:
+    event_type = "FAIL"
+
+  end_record = {
+                  "eventType": event_type,
+                  "eventTime": run_summary["end_time"],
+                  "job": run_summary["job"],
+                  "run": run_summary["run"],
+                  "inputs": run_summary["inputs"],
+                  "outputs": run_summary["outputs"],
+                  "producer": "https://meltano.com"
+                }
+
+  return start_record, end_record
+
+
+def convert_dict_to_array(d):
+  for key, value in d.items():
+
+    if isinstance(value["type"], list):
+      value["type"] = value["type"][0]
+
+    item = {
+      "name": key,
+      "type": value["type"],
+      "description": value.get("description")
     }
+    yield item
+
+
+def parse_logs(filepath, manifest_path):
 
   f = open(filepath, "r")
   m = json.load(open(manifest_path, "r"))
 
-  streams = []
-
   for line in f.readlines():
     j = json.loads(line)
+
+    if j.get("event") and re.match(r"Environment (.*) is active", j["event"]):
+        d = {
+          "producer": "https://meltano.com",
+          "producer_name": None, 
+          "consumer_name": None, 
+          "streams": [],
+          "inputs": [],
+          "outputs": [],
+          "run": {
+            "runId": str(uuid.uuid4()),
+            "metrics": {}
+          },
+          "job": {
+          "namespace": None,
+          "name": None,
+          },
+          "start_time": None, 
+          "end_time": None,
+        }
+
     if j.get("producer"):
       d["producer_name"] = j["string_id"]
     if j.get("consumer"):
@@ -62,15 +109,7 @@ def parse_logs(filepath, manifest_path):
     if j.get("timestamp"):
       d["end_time"] = j["timestamp"]
 
-    if j.get("producer"):
-      producer_config = get_values(j["string_id"], m["plugins"]["extractors"])
-      producer_name = j["string_id"]
-      
-    if j.get("consumer"):
-      consumer_config = get_values(j["string_id"], m["plugins"]["loaders"])
-      consumer_name = j["string_id"]
 
-    
     if "INFO METRIC:" in j.get("event"):
       config = get_values(j["string_id"], m["plugins"]["loaders"])
       name = j["string_id"]
@@ -104,8 +143,10 @@ def parse_logs(filepath, manifest_path):
       stream_name = parsed_json["stream"]
       if not stream_name in d["streams"]:
         d["streams"].append(stream_name)
-        schema = parsed_json["schema"]["properties"]
+        schema = list(convert_dict_to_array(parsed_json["schema"]["properties"]))
         input["name"] = stream_name
+        input["facets"]["schema"]["_producer"] = "meltano"
+        input["facets"]["schema"]["_schemaURL"] = "https://example.com"
         input["facets"]["schema"]["fields"] = schema
 
         if j.get("producer"):
@@ -117,48 +158,63 @@ def parse_logs(filepath, manifest_path):
           input["facets"]["config"] = config.copy()
           d["outputs"].append(input)
 
+    if "success" in j.keys():
+      d["success"] = j["success"]
+      d["job"]["namespace"] = "meltano"
+      d["job"]["name"] = f"{d['producer_name']}-to-{d['consumer_name']}"
+      yield d
+
+
   f.close()
-  start_record = {
-                  "eventType": "START", 
-                  "eventTime": d["start_time"],
-                  "run": {
-                    "runId": d["run"]["runId"],
-                  },
-                  "inputs": d["inputs"],
-                  "outputs": d["outputs"]
-                }
+  # start_record = {
+  #                 "eventType": "START", 
+  #                 "eventTime": d["start_time"],
+  #                 "run": {
+  #                   "runId": d["run"]["runId"],
+  #                 },
+  #                 "inputs": d["inputs"],
+  #                 "outputs": d["outputs"]
+  #               }
 
-  end_record = {
-                  "eventType": "COMPLETE",
-                  "eventTime": d["end_time"],
-                  "run": d["run"],
-                  "inputs": d["inputs"],
-                  "outputs": d["outputs"]
-                }
+  # end_record = {
+  #                 "eventType": "COMPLETE",
+  #                 "eventTime": d["end_time"],
+  #                 "run": d["run"],
+  #                 "inputs": d["inputs"],
+  #                 "outputs": d["outputs"]
+  #               }
 
 
-  return start_record, end_record
 
 if __name__ == "__main__":
 
   parser = argparse.ArgumentParser()
   parser.add_argument("--logs", help="The Meltano logs file", default="data/meltano.log")
   parser.add_argument("--manifest", help="The Meltano manifest file", default="data/manifest.json")
-  parser.add_argument("--output", help="The output file", default="openlineage.json")
+  parser.add_argument("--output", help="The output file", default="output.json")
   parser.add_argument("--print", help="Print the output to the console", default=False)
 
   args = parser.parse_args()
-  start_record, complete_record = parse_logs(args.logs, args.manifest)
+  results = parse_logs(args.logs, args.manifest)
 
-  if os.path.exists(args.output):
-    os.remove(args.output)
+  if os.path.exists("openlinage_" + args.output):
+    os.remove("openlinage_" + args.output)
 
-  with open(args.output, "w") as f:
-    f.write(json.dumps(start_record) + '\n')
-    f.write(json.dumps(complete_record) + '\n')
+  if os.path.exists("meltano_" + args.output):
+    os.remove("meltano_" + args.output)
 
-  if args.print:
-    print("******* START RECORD: *******")
-    print(json.dumps(start_record, indent=2))
-    print("******* COMPLETE RECORD: *******")
-    print(json.dumps(complete_record, indent=2))
+
+  openlineage_records = open("openlinage_" + args.output, "w")
+  meltano_summary = open("meltano_" + args.output, "w")
+
+  for result in results:
+    meltano_summary.write(json.dumps(result) + '\n')
+    openlineage_start, openlineage_complete = emit_openlineage_from_summary(result)
+    openlineage_records.write(json.dumps(openlineage_start) + '\n')
+    openlineage_records.write(json.dumps(openlineage_complete) + '\n')
+
+  # if args.print:
+  #   print("******* START RECORD: *******")
+  #   print(json.dumps(start_record, indent=2))
+  #   print("******* COMPLETE RECORD: *******")
+  #   print(json.dumps(complete_record, indent=2))
